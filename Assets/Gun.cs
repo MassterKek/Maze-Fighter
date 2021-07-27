0ff93e7bd094d5990dc51ce54bf7f7c7fa335d42
Carlos Jimenez <lamaravillasan@outlook.com>
2021-07-26 22:54:56 -0700

    Shooting Stones

---
+++ Assets/Gun.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject stone;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            shoot();
+        }
+    }   
+
+    public void shoot() {
+        Instantiate(stone, firePoint.position, firePoint.rotation);
+    }
+}
