85abafeb9e1a0a18cb4f6669d6c3a8b309a97bf9
MassterKek <trevorwardkun@gmail.com>
2021-08-03 23:49:56 -0400

    Implemented Item Pathfinding
    Implemented code that allows the AI to efficiently navigate to specific locations.

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -11,11 +11,15 @@ public class EnemyControl : MonoBehaviour
     GameObject currentTile;
     GameObject player;
     string direction = "down";
+    public string state = "Patrol";
     public int enemyPos = 99;
+    public int lastPos = 99;
     int pPos = 0;
-    float dirTime = 0;
-    float movTime = 0;
-    int health;
+    public float timer = 0.5f;
+    public float movTime = 0;
+    public int goal;
+    public int health;
+    public int weaponCount;
 
     void ChangeSprite(int dir)
     {
@@ -28,6 +32,7 @@ public class EnemyControl : MonoBehaviour
         {
             if(!isWall(enemyPos + 10) && enemyPos < 90 && (enemyPos + 10) != pPos)
             {
+                lastPos = enemyPos;
                 enemyPos += 10;
                 UpdatePosition();
             }
@@ -36,6 +41,7 @@ public class EnemyControl : MonoBehaviour
         {
             if(!isWall(enemyPos + 1) && (enemyPos%10) != 9 && (enemyPos + 1) != pPos)
             {
+                lastPos = enemyPos;
                 enemyPos += 1;
                 UpdatePosition();
             }
@@ -44,6 +50,7 @@ public class EnemyControl : MonoBehaviour
         {
             if(!isWall(enemyPos - 1) && (enemyPos%10) != 0 && (enemyPos - 1) != pPos)
             {
+                lastPos = enemyPos;
                 enemyPos -= 1;
                 UpdatePosition();
             }
@@ -52,6 +59,7 @@ public class EnemyControl : MonoBehaviour
         {
             if(!isWall(enemyPos - 10) && enemyPos >= 10 && (enemyPos - 10) != pPos)
             {
+                lastPos = enemyPos;
                 enemyPos -= 10;
                 UpdatePosition();
             }
@@ -75,6 +83,7 @@ public class EnemyControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        goal = 90;
         health = 100;
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         spriteRenderer.sortingOrder = 1;
@@ -83,7 +92,6 @@ public class EnemyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dirTime += Time.deltaTime;
         movTime += Time.deltaTime;
         UpdatePosition();
 
@@ -91,48 +99,322 @@ public class EnemyControl : MonoBehaviour
         PlayerControl cs = player.GetComponent<PlayerControl>();
         pPos = cs.playerPos;
 
-        if (dirTime > 2.0)
-        {
-            dirTime = 0;
-            int dice = Random.Range(1, 5);;
+        if(weaponCount==0)state="Scout";
+        else state="Patrol";
 
-            switch (dice)
-            {
-                case 1:
-                    ChangeSprite(0);
-                    direction = "up";
-                    break;
-                case 2:
-                    ChangeSprite(1);
-                    direction = "down";
-                    break;
-                case 3:
-                    ChangeSprite(2);
-                    direction = "left";
-                    break;
-                case 4:
-                    ChangeSprite(3);
-                    direction = "right";
-                    break;
-            }
+        switch(state)
+        {
+            case "Patrol":
+                UpdatePatrol();
+                break;
+            case "Scout":
+                UpdateScout();
+                break;
+            
         }
-        
-        if (movTime > 1.0) 
+
+    }
+
+    void changeDirection(int res)
+    {
+        ChangeSprite(res);
+
+        switch (res)
+        {
+            case 0:
+                direction = "up";
+                break;
+            case 1:
+                direction = "down";
+                break;
+            case 2:
+                direction = "left";
+                break;
+            case 3:
+                direction = "right";
+                break;
+        }
+    }
+
+    void UpdatePatrol()
+    {
+        if (movTime > timer*2)
         {
             movTime = 0;
+
+            int[] moves = new int[]{0,0,0,0};
+            int numMoves = 0;
+            if (upClear(enemyPos))
+            {
+                moves[0] = 1;
+                numMoves++;
+            }
+            if (downClear(enemyPos))
+            {
+                moves[1] = 1;
+                numMoves++;
+            }
+            if (leftClear(enemyPos))
+            {
+                moves[2] = 1;
+                numMoves++;
+            }
+            if (rightClear(enemyPos))
+            {
+                moves[3] = 1;
+                numMoves++;
+            }
+            
+            bool choosing = true;
+            int dice = 1;
+            while(choosing)
+            {
+                dice = Random.Range(0, 4);
+                if (moves[dice] == 1)
+			        choosing = false;
+                if (numMoves > 1 && nextTile(dice) == lastPos)
+                    choosing = true;
+                if(numMoves == 0)
+                    choosing = false;
+            }
+
+            changeDirection(dice);
             MovePlayer();
         }
+
+    }
+
+    void UpdateScout()
+    {
+        if(movTime > timer)
+        {
+            movTime = 0;
+
+            int curTile = enemyPos;
+
+            Queue bfs = new Queue();
+            bfs.Enqueue(curTile);
+            ArrayList passed= new ArrayList();
+
+            bool searching = true;
+            
+            //each index refers to a tile, and the value at that index represents
+            //the tile that preceded it in the path
+            int[] parent = new int[100];
+            parent[enemyPos] = lastPos;
+            while (searching)
+            {
+                if (upClear(curTile) && !passed.Contains(curTile+10))
+                {
+                    parent[curTile+10] = curTile;
+                    if(!bfs.Contains(curTile+10))
+                    {
+                        bfs.Enqueue(curTile+10);
+                    }
+                }
+                if (downClear(curTile) && !passed.Contains(curTile-10))
+                {
+                    parent[curTile-10] = curTile;
+                    if(!bfs.Contains(curTile-10))
+                    {
+                        bfs.Enqueue(curTile-10);
+                    }
+                }
+                if (leftClear(curTile) && !passed.Contains(curTile-1))
+                {
+                    parent[curTile-1] = curTile;
+                    if(!bfs.Contains(curTile-1))
+                    {
+                        bfs.Enqueue(curTile-1);
+                    }
+                }
+                if (rightClear(curTile) && !passed.Contains(curTile+1))
+                {
+                    parent[curTile+1] = curTile;
+                    if(!bfs.Contains(curTile+1))
+                    {
+                        bfs.Enqueue(curTile+1);
+                    }
+                }
+
+                passed.Add(curTile);
+                curTile = (int)bfs.Dequeue();
+                
+                if(curTile == goal)
+                {
+                    searching = false;
+                }
+
+            }
+            Debug.Log("curTile = " + curTile);
+            while(parent[curTile] != enemyPos)
+            {
+                curTile = parent[curTile];
+            }
+            Debug.Log("Move to " + curTile);
+
+            if (curTile%10 == enemyPos%10)
+                if (curTile > enemyPos)
+                    changeDirection(0);
+                else
+                    changeDirection(1);
+
+            if (curTile/10 == enemyPos/10)
+                if (curTile < enemyPos)
+                    changeDirection(2);
+                else
+                    changeDirection(3);
+            
+            MovePlayer();
+
+            if(enemyPos == goal)
+            {
+                weaponCount++;
+                state = "Patrol";
+            }
+        }
+    }
+
+
+    int nextTile(int dir)
+    {
+        int temp = enemyPos;
+        switch(dir)
+        {
+            case 0:
+                temp+=10;
+                break;
+            case 1:
+                temp-=10;
+                break;
+            case 2:
+                temp-=1;
+                break;
+            case 3:
+                temp+=1;
+                break;
+        }
+        return temp;
     }
 
     void UpdatePosition()
     {
         currentTile = GameObject.FindWithTag(""+enemyPos);
-        Debug.Log("Moving to tile: " + enemyPos);
+        //Debug.Log("Moving to tile: " + enemyPos);
         transform.position = currentTile.transform.position;
     }
 
     void TakeDamage (int damage) {
         health -= damage;
     }
+
+    bool isVisible()
+    {
+        bool foundStone = false;
+        if (inRow())
+        {	
+            if (toRight(pPos))
+            {
+                for (int i=enemyPos+1; i<pPos; i++)
+                {
+                    if (isWall(i))
+                        foundStone = true;
+                }
+            }
+            else
+            {
+                for (int i=pPos+1; i<enemyPos; i++)
+                {
+                    if (isWall(i))
+                        foundStone = true;
+                }
+            }
+        }
+        else if (inCol())
+        {
+            if (toAbove(pPos))
+            {
+                
+                for (int i=enemyPos+10; i<pPos; i+=10)
+                {
+                    if (isWall(i))
+                        foundStone = true;
+                }
+            }
+            else
+            {
+                for (int i=pPos+10; i<enemyPos; i+=10)
+                {
+                    if (isWall(i))
+                        foundStone = true;
+                }
+            }
+        }
+        return !foundStone;
+    }
+
+    bool inRow()
+    {
+        bool res = false;
+        if (pPos/10 == enemyPos/10)
+            res = true;
+        return res;
+    }
+
+    bool inCol()
+    {
+        bool res = false;
+        if (pPos%10 == enemyPos%10)
+            res = true;
+        return res;
+    }
+
+    int xPos(int location)
+    {
+        return location%10;
+    }
+
+    private int yPos(int location)
+    {
+        return location/10;
+    }
+
+    private bool toRight(int objPos)
+    {
+        bool res = false;
+        if (xPos(objPos) > xPos(enemyPos))
+            res = true;
+        return res;
+    }
+
+    bool toAbove(int objPos)
+    {
+        bool res = false;
+        if (yPos(objPos) > yPos(enemyPos))
+            res = true;
+        return res;
+    }
+
+    bool upClear(int objPos)
+    {
+        return ((!isWall(objPos + 10)) && (objPos + 10 <= 99) && (objPos + 10 != pPos));
+    }
+
+    bool downClear(int objPos)
+    {
+        return ((!isWall(objPos - 10)) && (objPos - 10 >= 0) && (objPos - 10 != pPos));
+    }
+
+    bool leftClear(int objPos)
+    {
+        return ((!isWall(objPos - 1)) && (objPos%10 - 1 >= 0) && (objPos - 1 != pPos));
+
+    }
+
+    bool rightClear(int objPos)
+    {
+        return ((!isWall(objPos + 1)) && (objPos%10 + 1 <= 9) && (objPos + 1 != pPos));
+    }
+
 
 }
