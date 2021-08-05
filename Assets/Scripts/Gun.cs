bf1a254f16ac91061b93780ec80f9244b30a6349
Carlos Jimenez <lamaravillasan@outlook.com>
2021-08-05 11:41:11 -0700

    Pickup
    Some progress

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,16 +6,34 @@ public class Gun : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject stone;
+    public GameObject cube;
+    public int cubes_picked = 0;
+    public bool isPicked;
 
     public void shoot() {
         GameObject bullet = Instantiate(stone, firePoint.position, firePoint.rotation);
         Destroy(bullet, 1.5f);
     }
 
+    public void pickup() {
+        cubes_picked++;
+        Debug.Log("Cubes Picked: " + cube);
+        Destroy(cube);
+    }
+
     void Update()
     {
+        if (isPicked) {
+            pickup();
+        }
         if (Input.GetMouseButtonDown(0)) {
             shoot();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D item) {
+        if (item.gameObject.name.Equals("Player")) {
+            isPicked = true;
         }
     }
 }
